fc5e7b37b2f841bfdb5abedfe8b852c73dac1e6b
Maicon Bonfim Izidoro <56212999+Maytech474@users.noreply.github.com>
2021-11-30 00:34:14 -0300

    Update loja.sql

--- loja.sql
+++ loja.sql
@@ -17,7 +17,7 @@ insert into ecommerce(produto, quantidade, tipo, setor, preço) values ("Vestido
 insert into ecommerce(produto, quantidade, tipo, setor, preço) values ("Moletom", 50, "Casual", "Unissex", 150.90);
 insert into ecommerce(produto, quantidade, tipo, setor, preço) values ("Corta vento", 50, "Manga longa", "Unissex", 49.90);
 insert into ecommerce(produto, quantidade, tipo, setor, preço) values ("Saia", 80, "Mid", "Feminino", 49.90);
-insert into ecommerce(produto, quantidade, tipo, setor, preço) values ("Calcinha", 50, "Fio Dental", "Unissex", 35.00);
+
 
 select * from ecommerce where preço > 500;
 select * from ecommerce where preço < 500;
